264158ce9b0dbf473ac2ca1477409f748bf334d1
Greg Newby <arinewby@ymail.com>
2022-11-10 16:41:51 -0600

    d flex container fix, secretQ session storage, and register blank flag

--- banking-app/src/app/login/login.component.ts
+++ banking-app/src/app/login/login.component.ts
@@ -99,6 +99,8 @@ export class LoginComponent implements OnInit {
               sessionStorage.setItem('customer-password',this.users[i].password);
               sessionStorage.setItem('customer-phone',this.users[i].phone);
               sessionStorage.setItem('customer-status',this.users[i].status);
+              sessionStorage.setItem('customer-secretQuestion', this.users[i].secretQuestion);
+              sessionStorage.setItem('customer-secretAnswer', this.users[i].secretAnswer);
               this.router.navigate(['/profile']);
               break; //braks out of loop once finished it will route to home
             } else{
